6d25f3f70fc13ccd2612101e1502e5be166d9b30
Duan <1393670327@qq.com>
2024-09-27 22:55:25 +0800

    reflection

---
+++ prac_02/REFLECTION.py
@@ -0,0 +1,9 @@
+"""
+1. all week2 video
+2. 9am   home   1-2h
+3. follow the steps in class
+4. add more practice on spare time
+5. I need to change something in after-class review
+6. sometimes I write a py file separately, and I need to bring them together
+7. after learning something in class, I can easily remember single examples.
+"""
