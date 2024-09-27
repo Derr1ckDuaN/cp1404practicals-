fd1207b25a5d1cd478191b57d1f993adca15250b
Duan <1393670327@qq.com>
2024-09-27 22:09:55 +0800

    use two functions to write this code

---
+++ prac_02/temperatures.py
@@ -0,0 +1,7 @@
+def shift_celsius_to_fahrenheit(celsius):
+    """convert celsius to fahrenheit"""
+    return (celsius * 9/5) + 32
+
+def shift_fahrenheit_to_celsius(fahrenheit):
+    """convert fahrenheit to celsius"""
+    return (fahrenheit - 32) *5/9
